86e35ae9ff7f683bf95a5c3b12d906daee0b9be5
adam m pere <adampere001@gmail.com>
2015-08-26 21:05:15 -0400

    simple layout, index + project pages

---
+++ config.rb
@@ -0,0 +1,20 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "develop/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+fonts_dir = "assets/fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = true
+color_output = true
+
+preferred_syntax = :scss
